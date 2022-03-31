9922c432fbed6d5894b7ecc3f44c11055977a06a
saintrabby <saintrabby@gmail.com>
2022-03-31 20:26:07 +0900

    c
    c

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyClkcCvtKFxs8hRVOxNBmkI76XvADxGPro",
-  authDomain: "hh99-react-7855e.firebaseapp.com",
-  projectId: "hh99-react-7855e",
-  storageBucket: "hh99-react-7855e.appspot.com",
-  messagingSenderId: "657733321219",
-  appId: "1:657733321219:web:8e940849c9371262798436",
-  measurementId: "G-RB5ZDJLZXW"
 };
 
 // Initialize Firebase
